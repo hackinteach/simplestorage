473f704c60785ef9eb6193ff98e751a92bf86744
Nuttapat Koonarangsri <webmaster@hackinteach.com>
2018-09-12 14:47:28 +0700

    add db config

--- src/Mongo/Mongo.go
+++ src/Mongo/Mongo.go
@@ -1,16 +1,34 @@
 package Mongo
 
 import (
-	"fmt"
+	. "../BucketManagement"
 	"gopkg.in/mgo.v2"
 )
 
-func Connect(
-	info mgo.DialInfo){
-		connectString := fmt.Sprintf("mongodb://%s:%s@%s:@%d/%s",user,password,host,port,db)
-	session, err := mgo.Dial(connectString)
-	if err != nil{
+var (
+	session          *mgo.Session
+	bucketCollection *mgo.Collection
+	objectCollection *mgo.Collection
+)
 
+const (
+	DB = "SimpleStorage"
+)
+
+func init() {
+	var err error
+	session, err = mgo.Dial("localhost")
+
+	if err != nil {
+		panic(err)
 	}
 
+	db := session.DB(DB)
+	bucketCollection = db.C("Bucket")
+	objectCollection = db.C("Object")
+}
+
+func AddBucket(bucket Bucket) (bool) {
+	err := bucketCollection.Insert(bucket)
+	return err != nil
 }
