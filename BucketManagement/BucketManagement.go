fb78179335ef629042252b5c2b835af3f649fb28
Nuttapat Koonarangsri <webmaster@hackinteach.com>
2018-09-12 09:17:49 +0700

    create struct Bucket

--- BucketManagement/BucketManagement.go
+++ BucketManagement/BucketManagement.go
@@ -6,10 +6,15 @@ import (
 	"net/http"
 )
 
+type Bucket struct {
+	Name string 	`json:"bucketName"`
+	Created int64	`json:"created"`
+	Modified int64	`json:"modified"`
+}
+
 func CreateBucket(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
-	bucketName := vars["bucketName"]
-	json.NewEncoder(w).Encode(bucketName)
+	json.NewEncoder(w).Encode(vars)
 }
 
 func DeleteBucket(w http.ResponseWriter, r *http.Request) {
