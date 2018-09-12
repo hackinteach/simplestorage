db8c44a07ab83d41999c3389e2aa0ad8f1cf5c76
Nuttapat Koonarangsri <webmaster@hackinteach.com>
2018-09-12 09:19:36 +0700

    add func for pattern check

--- Misc/Misc.go
+++ Misc/Misc.go
@@ -1,1 +1,13 @@
 package Misc
+
+import (
+	"regexp"
+)
+
+func ValidatePattern(str string)(bool){
+	matched, err := regexp.MatchString("^[1-9][0-9]{0,3}0?",str)
+	if err != nil{
+
+	}
+	return matched
+}
