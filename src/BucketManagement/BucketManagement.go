6595c1da36682f9c75c04a9df0b53a9dabe897b0
Nuttapat Koonarangsri <webmaster@hackinteach.com>
2018-09-12 14:47:46 +0700

    add bson identifier

--- src/BucketManagement/BucketManagement.go
+++ src/BucketManagement/BucketManagement.go
@@ -3,13 +3,15 @@ package BucketManagement
 import (
 	"encoding/json"
 	"github.com/gorilla/mux"
+	"gopkg.in/mgo.v2/bson"
 	"net/http"
 )
 
 type Bucket struct {
-	Name string 	`json:"bucketName"`
-	Created int64	`json:"created"`
-	Modified int64	`json:"modified"`
+	ID bson.ObjectId `bson:"_id,omitempty"`
+	Name string 	`bson:"name" json:"bucketName"`
+	Created int64	`bson:"created" json:"created"`
+	Modified int64	`bson:"modified" json:"modified"`
 }
 
 func CreateBucket(w http.ResponseWriter, r *http.Request) {
