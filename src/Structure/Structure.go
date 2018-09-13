816321485e76d919b7907d8b498032d924b9460a
Nuttapat Koonarangsri <webmaster@hackinteach.com>
2018-09-14 00:33:11 +0700

    refactor code

--- src/Structure/Structure.go
+++ src/Structure/Structure.go
@@ -1,1 +1,10 @@
 package Structure
+
+import "gopkg.in/mgo.v2/bson"
+
+type Bucket struct {
+	ID	 		bson.ObjectId	`bson:"_id,omitempty"`
+	Name 		string 			`bson:"name" json:"bucketName"`
+	Created 	int64			`bson:"created" json:"created"`
+	Modified 	int64			`bson:"modified" json:"modified"`
+}
