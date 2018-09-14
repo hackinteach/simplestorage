00e213c47e88a6fd9479f28f1ed794b693cc5a28
Nuttapat Koonarangsri <webmaster@hackinteach.com>
2018-09-14 21:55:44 +0700

    add object and part struct

--- Structure/Structure.go
+++ Structure/Structure.go
@@ -14,3 +14,18 @@ type TempBucket struct {
 	Created 	int64			`json:"created"`
 	Modified 	int64			`json:"modified"`
 }
+
+type Object struct {
+	ID			bson.ObjectId	`bson:"_id"`
+	Name 		string			`bson:"name",json:"name"`
+	ETag		string			`bson:"etag",json:"etag"`
+	Parts		[]Part			`bson:"parts",json:"part"`
+	Length		int				`bson:"length",json:"length"`
+}
+
+type Part struct {
+	ID			bson.ObjectId	`bson:"_id"`
+	Number 		int				`bson:"number",json:"number"`
+	MD5 		string			`bson:"md5",json:"md5"`
+	Size		int				`bson:"size",json:"size"`
+}
