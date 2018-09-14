32e2fbc70f36a20055e1d6079a3c2b5f997e4e16
Nuttapat Koonarangsri <webmaster@hackinteach.com>
2018-09-14 18:52:06 +0700

    create return output

--- BucketManagement/BucketManagement.go
+++ BucketManagement/BucketManagement.go
@@ -1,29 +1,46 @@
 package BucketManagement
 
 import (
-	. "../Misc"
-	. "../Mongo"
-	. "../Structure"
+	"encoding/json"
 	"github.com/mitchellh/mapstructure"
+	"log"
 	"net/http"
+	. "simplestorage/Misc"
+	. "simplestorage/Mongo"
+	. "simplestorage/Structure"
 	"strings"
 )
 
 func CreateBucket(w http.ResponseWriter, r *http.Request) {
-
+	w.Header().Set("content-type","application/json")
 	var bucket Bucket
-	var tmp map[string]interface{}
-
-	tmp = make(map[string]interface{})
 
 	bucketName := strings.Replace(r.URL.Path,"/","",1)
-	tmp["Name"] = bucketName
-	tmp["Created"] = GetTime()
-	tmp["Modified"] = GetTime()
+
+	var tmp map[string]interface{}
+	tmp = make(map[string]interface{})
+	tmp["name"] = bucketName
+	tmp["created"] = GetTime()
+	tmp["modified"] = GetTime()
+
 	mapstructure.Decode(tmp,&bucket)
+
+
+
 	if ! CheckBucketExist(bucketName){
+		// Bucket can be create
+		log.Printf("Creating %s",bucketName)
 		AddBucket(bucket)
+		log.Printf("Added %s to MongoDB",bucketName)
 		MakeDirectory(bucketName)
+
+		var tmpBucket TempBucket
+		mapstructure.Decode(tmp,&tmpBucket)
+		json.NewEncoder(w).Encode(tmpBucket)
+		w.WriteHeader(200)
+	}else{
+		log.Printf("Bucket already exists")
+		w.WriteHeader(400)
 	}
 }
 
