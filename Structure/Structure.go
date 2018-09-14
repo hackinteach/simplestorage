589a59f5c406a955fa3d17a8f23ea8e49dd47d24
Nuttapat Koonarangsri <webmaster@hackinteach.com>
2018-09-14 18:52:38 +0700

    add tmp bucket struct

--- Structure/Structure.go
+++ Structure/Structure.go
@@ -4,7 +4,13 @@ import "gopkg.in/mgo.v2/bson"
 
 type Bucket struct {
 	ID	 		bson.ObjectId	`bson:"_id,omitempty"`
-	Name 		string 			`bson:"name" json:"bucketName"`
+	Name 		string 			`bson:"name" json:"name"`
 	Created 	int64			`bson:"created" json:"created"`
 	Modified 	int64			`bson:"modified" json:"modified"`
+}
+
+type TempBucket struct {
+	Name 		string 			`json:"name"`
+	Created 	int64			`json:"created"`
+	Modified 	int64			`json:"modified"`
 }
