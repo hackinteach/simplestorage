00969a9a6dcec3e6728381b27dae32804e074735
Nuttapat Koonarangsri <webmaster@hackinteach.com>
2018-09-16 00:31:12 +0700

    done create upload ticket

--- ObjectManagement/ObjectManagement.go
+++ ObjectManagement/ObjectManagement.go
@@ -3,14 +3,43 @@ package ObjectManagement
 import (
 	"encoding/json"
 	"github.com/gorilla/mux"
+	"github.com/mitchellh/mapstructure"
+	"log"
 	"net/http"
 	. "simplestorage/Misc"
+	. "simplestorage/Mongo"
+	. "simplestorage/Structure"
 )
 
 func CreateTicket(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
-	bucketName := vars["bucketName"]
-	json.NewEncoder(w).Encode(bucketName)
+	bucketName := GetBucketName(r)
+	log.Printf("Bucketname: %s",bucketName)
+	objectName := GetObjectName(r)
+
+	buckExist := CheckBucketExist(bucketName)
+	objExist := CheckObjectExist(objectName)
+
+	if buckExist && !objExist {
+		MakeObjectDirectory(bucketName,objectName)
+		//@TODO update DB
+		var object Object
+
+		var temp map[string]interface{}
+		temp = make(map[string]interface{})
+
+		temp["name"] = objectName
+		temp["bucket"] = bucketName
+
+		mapstructure.Decode(temp,&object)
+
+		CreateObject(object)
+
+		w.Header().Set("Content-Type","application/json")
+		json.NewEncoder(w).Encode(object)
+		w.WriteHeader(200)
+	}else{
+		w.WriteHeader(400)
+	}
 }
 
 func UploadAll(w http.ResponseWriter, r *http.Request) {
