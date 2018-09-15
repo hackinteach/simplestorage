b6f0633ef79f37a1d083cf66a1509392b7ee9946
Nuttapat Koonarangsri <webmaster@hackinteach.com>
2018-09-16 00:26:41 +0700

    add mapstructure tag, create tmpobject strucut

--- Structure/Structure.go
+++ Structure/Structure.go
@@ -11,22 +11,32 @@ type Bucket struct {
 }
 // for returning purpose
 type TempBucket struct {
-	Name 		string 			`json:"name"`
-	Created 	int64			`json:"created"`
-	Modified 	int64			`json:"modified"`
+	Name 		string 			`json:"name" mapstructure:"name"`
+	Created 	int64			`json:"created" mapstructure:"created"`
+	Modified 	int64			`json:"modified" mapstructure:"modified"`
 }
 
 type Object struct {
 	ID			bson.ObjectId	`bson:"_id,omitempty"`
-	Name 		string			`bson:"name",json:"name"`
-	ETag		string			`bson:"etag",json:"etag"`
-	Parts		[]Part			`bson:"parts",json:"part"`
-	Length		int				`bson:"length",json:"length"`
+	Name 		string			`bson:"name" json:"name"`
+	ETag		string			`bson:"etag" json:"etag"`
+	Parts		[]Part			`bson:"parts" json:"part"`
+	Length		int				`bson:"length" json:"length"`
+	Bucket		string			`bson:"bucket" json:"bucket"`
+	Created 	int64			`bson:"created" json:"created"`
+	Modified 	int64			`bson:"modified" json:"modified"`
+}
+
+type TempObject struct {
+	Name 		string			`bson:"name" json:"name" mapstructure:"name"`
+	ETag		string			`bson:"etag" json:"eTag"  mapstructure:"eTag"`
+	Created 	int64			`json:"created" mapstructure:"created"`
+	Modified 	int64			`json:"modified" mapstructure:"modified"`
 }
 
 type Part struct {
-	//ID			bson.ObjectId	`bson:"_id,omitempty"`
-	Number 		int				`bson:"number",json:"number"`
-	MD5 		string			`bson:"md5",json:"md5"`
-	Size		int				`bson:"size",json:"size"`
+	ID			bson.ObjectId	`bson:"_id,omitempty"`
+	Number 		int				`bson:"number" json:"number"`
+	MD5 		string			`bson:"md5" json:"md5"`
+	Size		int				`bson:"size" json:"size"`
 }
