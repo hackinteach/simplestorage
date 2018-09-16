ff1c477de52b514cd8608c8743432d01cfcb5c2e
Nuttapat Koonarangsri <webmaster@hackinteach.com>
2018-09-16 21:05:18 +0700

    finish write file with md5

--- Misc/Misc.go
+++ Misc/Misc.go
@@ -1,7 +1,11 @@
 package Misc
 
 import (
+	"bytes"
+	"crypto/md5"
+	"fmt"
 	"github.com/gorilla/mux"
+	"io"
 	"io/ioutil"
 	"log"
 	"net/http"
@@ -17,32 +21,32 @@ const (
 	BucketPath = "buckets/"
 )
 
-func ValidatePattern(str string, pat string)(bool){
-	matched, _ := regexp.MatchString(pat,str)
+func ValidatePattern(str string, pat string) (bool) {
+	matched, _ := regexp.MatchString(pat, str)
 	return matched
 }
 
-func GetTime()(int64){
+func GetTime() (int64) {
 	return time.Now().UnixNano() / 1000000
 }
 
-func MakeBucketDirectory(name string)(bool){
-	var fullPath = filepath.Join(BucketPath,name)
+func MakeBucketDirectory(name string) (bool) {
+	var fullPath = filepath.Join(BucketPath, name)
 	//log.Printf("path: %s",fullPath)
 	err := os.MkdirAll(fullPath, 511)
-	exec.Command("chmod","777",fullPath)
+	exec.Command("chmod", "777", fullPath)
 	return err == nil
 }
 
-func MakeObjectDirectory(bucket string, name string)(bool){
-	var fullPath = filepath.Join(BucketPath,bucket,"/",name)
-	err := os.Mkdir(fullPath,511)
-	exec.Command("chmod","777",fullPath)
+func MakeObjectDirectory(bucket string, name string) (bool) {
+	var fullPath = filepath.Join(BucketPath, bucket, "/", name)
+	err := os.Mkdir(fullPath, 511)
+	exec.Command("chmod", "777", fullPath)
 	return err == nil
 }
 
-func RemoveDirectory(name string)(bool){
-	var fullPath = filepath.Join(BucketPath,name)
+func RemoveDirectory(name string) (bool) {
+	var fullPath = filepath.Join(BucketPath, name)
 
 	err := os.Remove(fullPath)
 	log.Print("Removing")
@@ -50,21 +54,30 @@ func RemoveDirectory(name string)(bool){
 	return err == nil
 }
 
-func GetBucketName(r *http.Request)(string) {
+func GetBucketName(r *http.Request) (string) {
 	return strings.ToLower(mux.Vars(r)["bucketName"])
 }
 
-func GetObjectName(r *http.Request)(string){
+func GetObjectName(r *http.Request) (string) {
 	return strings.ToLower(mux.Vars(r)["objectName"])
 }
 
-func WriteFile(f []byte, filename string, object string, bucket string)(bool){
+func WriteFile(f []byte, filename string, object string, bucket string) (MD5 string, Error error) {
+	hash := md5.New()
+
 	permission := os.FileMode(0755).Perm()
-	path := filepath.Join(BucketPath,"/",bucket,"/",object,"/",filename)
-	err := ioutil.WriteFile(path,f,permission)
+	path := filepath.Join(BucketPath, "/", bucket, "/", object, "/", filename)
+	//file, _ := ioutil.ReadAll(f)
+
+	err := ioutil.WriteFile(path, f, permission)
 	if err != nil {
-		log.Printf("Cannot write file %s",filename)
-		return false
+		log.Print(err)
+		return "", err
 	}
-	return true
+	reader := bytes.NewReader(f)
+	if _, err := io.Copy(hash, reader); err != nil {
+		return "", err
+	}
+	checksum := fmt.Sprintf("%x",hash.Sum([]byte{}))
+	return checksum, nil
 }
