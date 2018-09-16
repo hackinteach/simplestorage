7b40535f5c5b4c443d74bca549fd4a3f4a130945
Nuttapat Koonarangsri <webmaster@hackinteach.com>
2018-09-16 16:54:59 +0700

    use camelCase

--- Structure/Structure.go
+++ Structure/Structure.go
@@ -20,9 +20,9 @@ type Object struct {
 	ID			bson.ObjectId	`bson:"_id,omitempty"`
 	Name 		string			`bson:"name" json:"name"`
 	ETag		string			`bson:"etag" json:"etag"`
-	Parts		[]Part			`bson:"parts" json:"part"`
 	Length		int				`bson:"length" json:"length"`
 	Bucket		string			`bson:"bucket" json:"bucket"`
+	Completed	bool			`bson:"completed" json:"completed"`
 	Created 	int64			`bson:"created" json:"created"`
 	Modified 	int64			`bson:"modified" json:"modified"`
 }
@@ -36,9 +36,10 @@ type TempObject struct {
 
 type Part struct {
 	ID			bson.ObjectId	`bson:"_id,omitempty"`
-	Number 		int				`bson:"number" json:"number"`
+	Number 		string				`bson:"number" json:"number"`
 	MD5 		string			`bson:"md5" json:"md5"`
-	Size		int				`bson:"size" json:"size"`
+	Size		string				`bson:"size" json:"size"`
+	Object		string			`bson:"object" json:"object"`
 }
 
 var ERROR = map[string]string {
@@ -49,6 +50,6 @@ var ERROR = map[string]string {
 	"InvalidBucket": "InvalidBucket",
 }
 
-const PART_NUM_PATTERN = `^([1-9][0-9]{0,3}|10000)$`
-const OBJ_NAME_PATTERN = `^(?!\.).[ \. | \_ | -| a-z | 0-9]*`
-const BUCK_NAME_PATTERN = `(^(?!\.)([a-z|1-9|\-|\_]){2,})`
+const PartNumPattern = `^([1-9][0-9]{0,3}|10000)$`
+const ObjNamePattern = `^(?!\.).[ \. | \_ | -| a-z | 0-9]*`
+const BuckNamePattern = `(^(?!\.)([a-z|1-9|\-|\_]){2,})`
